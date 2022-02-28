25f00cb8d383ce4fc2062b33d5636d1911e918ed
Shoya-Y <touka.nt.gh@gmail.com>
2022-03-01 01:42:30 +0900

    remove unused

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,4 @@
-import IconWastar from 'public/assets/images/icon_wastar.png'
 import LogoAstar from 'public/assets/images/logo_astar.png'
 
 export * from './symbols'
-export { IconWastar, LogoAstar }
+export { LogoAstar }
