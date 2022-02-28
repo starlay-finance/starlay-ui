9acc2b3c92addd75e2dd32e3b13c59d23505da65
Shumpei Koike <shunpei42ba@gmail.com>
2022-02-28 21:01:40 +0800

    fix mobile text

--- src/components/screens/Sorry/reasons.ts
+++ src/components/screens/Sorry/reasons.ts
@@ -2,7 +2,7 @@ import { t } from '@lingui/macro'
 import { FromQuery, SorryReason } from 'src/types/page'
 
 const SORRY_TEXT: { [key in SorryReason]: string } = {
-  'mobile-not-supported': t`The Starlay Protocol front-end system does not support mobile access, so please access the site from a PC.`,
+  'mobile-not-supported': t`Starlay Finance does not support mobile access. Please connect to this website using your PC.`,
 }
 
 export const reasonToMessage = (reason: FromQuery<SorryReason>) =>
