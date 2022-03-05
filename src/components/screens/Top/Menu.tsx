55100039e1766ab760f1c0b4f4ef773bd7f99831
Shoya-Y <touka.nt.gh@gmail.com>
2022-03-05 21:42:47 +0900

    fix background

--- src/components/screens/Top/Menu.tsx
+++ src/components/screens/Top/Menu.tsx
@@ -1,10 +1,11 @@
 import { t } from '@lingui/macro'
-import { VFC } from 'react'
+import { useEffect, VFC } from 'react'
 import {
   IconClose,
   IconDiscord,
   IconGithub,
   IconMedium,
+  IconProtocol,
   IconTelegram,
   IconTwitter,
 } from 'src/assets/svgs'
@@ -12,6 +13,7 @@ import { Link } from 'src/components/elements/Link'
 import { IconLink } from 'src/components/parts/Link'
 import { purple, trueWhite } from 'src/styles/colors'
 import { fontWeightBold, fontWeightRegular } from 'src/styles/font'
+import { disableScroll, enableScroll } from 'src/utils/handleScroll'
 import {
   APP,
   BUG_BOUNTY,
@@ -29,31 +31,43 @@ import styled, { css } from 'styled-components'
 export const MobileMenu: VFC<{
   isOpen: boolean
   close: VoidFunction
-}> = ({ isOpen, close }) => (
-  <MenuContainer isOpen={isOpen}>
-    <MenuHeaderDiv>
-      <h3>{t`Menu`}</h3>
-      <button onClick={close}>
-        <IconClose />
-      </button>
-    </MenuHeaderDiv>
-    <Nav>
-      <Link href={APP}>{t`Launch App`}</Link>
-      <Link href={DISCORD}>{t`Discord`}</Link>
-      <Link href={DOCS}>{t`Docs`}</Link>
-      <Link href={GOVERNANCE}>{t`Governance`}</Link>
-      <Link href={BUG_BOUNTY}>{t`Bug Bounty`}</Link>
-    </Nav>
-    <IconLinks>
-      <IconLink Icon={IconTwitter} href={TWITTER} aria-label={t`Twitter`} />
-      <IconLink Icon={IconDiscord} href={DISCORD} aria-label={t`Discord`} />
-      <IconLink Icon={IconTelegram} href={TELEGRAM} aria-label={t`Telegram`} />
-      <IconLink Icon={IconMedium} href={MEDIUM} aria-label={t`Medium`} />
-      <IconLink Icon={IconGithub} href={GITHUB} aria-label={t`Github`} />
-    </IconLinks>
-  </MenuContainer>
-)
-
+}> = ({ isOpen, close }) => {
+  useEffect(() => {
+    if (isOpen) disableScroll()
+    else enableScroll()
+  }, [isOpen])
+  return (
+    <MenuContainer isOpen={isOpen}>
+      <BgIcon>
+        <IconProtocol />
+      </BgIcon>
+      <MenuHeaderDiv>
+        <h3>{t`Menu`}</h3>
+        <button onClick={close}>
+          <IconClose />
+        </button>
+      </MenuHeaderDiv>
+      <Nav>
+        <Link href={APP}>{t`Launch App`}</Link>
+        <Link href={DISCORD}>{t`Discord`}</Link>
+        <Link href={DOCS}>{t`Docs`}</Link>
+        <Link href={GOVERNANCE}>{t`Governance`}</Link>
+        <Link href={BUG_BOUNTY}>{t`Bug Bounty`}</Link>
+      </Nav>
+      <IconLinks>
+        <IconLink Icon={IconTwitter} href={TWITTER} aria-label={t`Twitter`} />
+        <IconLink Icon={IconDiscord} href={DISCORD} aria-label={t`Discord`} />
+        <IconLink
+          Icon={IconTelegram}
+          href={TELEGRAM}
+          aria-label={t`Telegram`}
+        />
+        <IconLink Icon={IconMedium} href={MEDIUM} aria-label={t`Medium`} />
+        <IconLink Icon={IconGithub} href={GITHUB} aria-label={t`Github`} />
+      </IconLinks>
+    </MenuContainer>
+  )
+}
 const Nav = styled.nav`
   display: flex;
   flex-direction: column;
@@ -82,6 +96,22 @@ const MenuHeaderDiv = styled.div`
   button {
     width: 16px;
     height: 16px;
+  }
+`
+const BgIcon = styled.div`
+  position: absolute;
+  z-index: -1;
+  bottom: -72px;
+  right: -116px;
+  width: 400px;
+  height: 400px;
+  opacity: 0.02;
+  svg {
+    width: 100%;
+    height: 100%;
+    path {
+      fill: ${trueWhite};
+    }
   }
 `
 const MenuContainer = styled.div<{ isOpen: boolean }>`
