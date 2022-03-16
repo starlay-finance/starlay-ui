2f6bdbc6a9dac7f9c1b7848bcd966a1e7f2795d1
Shoya-Y <touka.nt.gh@gmail.com>
2022-03-17 00:22:06 +0900

    fetch latest wallet balance

--- src/components/screens/Dashboard/modals/SuggestModal/index.tsx
+++ src/components/screens/Dashboard/modals/SuggestModal/index.tsx
@@ -3,6 +3,7 @@ import { BigNumber } from '@starlay-finance/math-utils'
 import { VFC } from 'react'
 import { DefaultModalContent } from 'src/components/parts/Modal/base'
 import { ModalContentProps, useModalDialog } from 'src/hooks/useModal'
+import { useWalletBalance } from 'src/hooks/useWalletBalance'
 import { css } from 'styled-components'
 import { SuggestModalBody, SuggestModalBodyProps } from './Body'
 
@@ -16,12 +17,19 @@ export const Suggest: VFC<
       }
     }
 > = ({ arthswapPair, ...props }) => {
+  const { data: balance } = useWalletBalance()
   const symbol = props.asset.displaySymbol || props.asset.symbol
   return (
     <DefaultModalContent
       headerNode={t`Comparing ${symbol} APYs for Next Action`}
       headerStyle={headerStyle}
-      bodyNode={<SuggestModalBody {...props} arthswapPair={arthswapPair} />}
+      bodyNode={
+        <SuggestModalBody
+          {...props}
+          inWallet={(balance && balance[props.asset.symbol]) || props.inWallet}
+          arthswapPair={arthswapPair}
+        />
+      }
       closeModal={props.close}
     />
   )
