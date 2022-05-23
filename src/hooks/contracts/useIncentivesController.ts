1904448fc35f553baf0d6597fb0b4f4a14723195
qudacki <touka.nt.gh@gmail.com>
2022-05-23 15:56:30 +0900

    enable to claim uncalculated rewards

--- src/hooks/contracts/useIncentivesController.ts
+++ src/hooks/contracts/useIncentivesController.ts
@@ -1,9 +1,11 @@
 import { ethers } from 'ethers'
-import { getMarketConfig, getNetworkConfig } from 'src/libs/config'
+import { getMarketConfig } from 'src/libs/config'
 import { incentivesControllerContract } from 'src/libs/incentives-controller'
 import { EthereumAddress } from 'src/types/web3'
 import useSWRImmutable from 'swr/immutable'
+import { useMarketData } from '../useMarketData'
 import { useStaticRPCProvider } from '../useStaticRPCProvider'
+import { useUserData } from '../useUserData'
 import { useTxHandler } from './txHandler'
 
 export const useIncentivesController = (
@@ -15,21 +17,33 @@ export const useIncentivesController = (
     provider && ['incentivescontroller', provider.chainId],
     () => incentivesControllerContract(provider!),
   )
-
+  const { data: marketData } = useMarketData()
+  const { data: userData } = useUserData()
   const { handleTx } = useTxHandler()
 
   const claim = async () => {
     if (!incentivesController || !account || !signer)
       throw new Error('Unexpected state')
-    const { rewardToken } = getNetworkConfig(provider!.chainId)
     const {
       addresses: { INCENTIVES_CONTROLLER },
     } = getMarketConfig(provider!.chainId)
+    const assets =
+      !userData || !marketData
+        ? []
+        : marketData.assets.flatMap(
+            ({ symbol, lTokenAddress, vdTokenAddress }) => {
+              const asset = userData.balanceByAsset[symbol]
+              const results = []
+              if (!asset.deposited.isZero()) results.push(lTokenAddress)
+              if (!asset.borrowed.isZero()) results.push(vdTokenAddress)
+              return results
+            },
+          )
     return handleTx(
       await incentivesController.claimRewards({
         user: account,
         incentivesControllerAddress: INCENTIVES_CONTROLLER,
-        assets: [rewardToken.underlyingAsset],
+        assets,
       }),
       signer,
     )
