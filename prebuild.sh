d21154f286aefac9304457baa98d49015e4f566a
qudacki <qudacki@gmail.com>
2023-03-22 14:58:33 +0900

    fix script to work with GNU

--- prebuild.sh
+++ prebuild.sh
@@ -2,5 +2,5 @@
 
 echo "Skip type checking in 'src/libs/polkadot/__generated__/**/*.ts'."
 for file in $(find  src/libs/polkadot/__generated__ -name \*.ts); do
-  sed -i '' '1s/^/\/\/ @ts-nocheck\n/' "${file}"
+  sed -i '1s/^/\/\/ @ts-nocheck\n/' "${file}"
 done
