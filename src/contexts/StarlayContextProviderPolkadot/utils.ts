98f6902a208b48641cf85f974da74d59adcafd19
qudacki <qudacki@gmail.com>
2023-03-23 22:50:26 +0900

    enable to sign transaction

--- src/contexts/StarlayContextProviderPolkadot/utils.ts
+++ src/contexts/StarlayContextProviderPolkadot/utils.ts
@@ -6,16 +6,18 @@ export const executeTx = async (
   account: string | undefined,
   signer: Signer | undefined,
 ) => {
-  if (!account || !signer?.signPayload) throw new Error('Unexpected state')
+  if (!account) throw new Error('Unexpected state')
   const tx = await item.tx()
-  const payload = JSON.parse(tx.toJSON())
-  const signature = await signer.signPayload(payload)
-  tx.addSignature(account, signature.signature, payload)
   return new Promise<void>((res, rej) => {
-    tx.send(({ isError }) =>
-      isError
-        ? rej('Transaction Failed') // TODO
-        : res(),
-    )
+    try {
+      tx.signAndSend(account, { signer }, ({ isError }) => {
+        if (!isError) return res()
+        // TODO
+        return rej('Transaction Failed')
+      })
+    } catch (e) {
+      // TODO
+      return rej(e)
+    }
   })
 }
