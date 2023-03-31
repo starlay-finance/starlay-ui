20359b04fedd5172742c4c8babae74c9b41aa377
qudacki <qudacki@gmail.com>
2023-03-31 17:39:28 +0900

    fix to match calculation in the contract

--- src/libs/data-provider-polkadot/utils.ts
+++ src/libs/data-provider-polkadot/utils.ts
@@ -5,7 +5,6 @@ import {
   valueToBigNumber,
   valueToZDBigNumber,
 } from '@starlay-finance/math-utils'
-import { BN_ZERO } from 'src/utils/number'
 
 const MILLISECONDS_PER_YEAR = 60 * 60 * 24 * 365 * 1000
 const DEFAULT_SCALE = 18
@@ -21,37 +20,28 @@ export const toString = (maybeString: string | number[]) => {
   return Buffer.from(hexStr.replace('0x', ''), 'hex').toString('utf-8')
 }
 
-export const toAPY = (ratePerMilliSec: ReturnNumber) => {
-  const ratePerMilliSecBn = toBigNumber(
-    ratePerMilliSec,
+export const toAPY = (_ratePerMilliSec: ReturnNumber) => {
+  const ratePerMilliSec = toBigNumber(
+    _ratePerMilliSec,
     UNIT_DECIMALS - DEFAULT_SCALE,
   )
-  return ratePerMilliSecBn.isZero()
-    ? BN_ZERO
-    : unitPow(ratePerMilliSecBn.plus(UNIT), MILLISECONDS_PER_YEAR)
-        .minus(UNIT)
-        .shiftedBy(-UNIT_DECIMALS)
+  const exp = valueToZDBigNumber(MILLISECONDS_PER_YEAR)
+  const expMinusOne = exp.minus(1)
+  const expMinusTwo = exp.minus(2)
+  const basePowerTwo = unitMul(ratePerMilliSec, ratePerMilliSec)
+  const basePowerThree = unitMul(basePowerTwo, ratePerMilliSec)
+  const secondTerm = exp.times(expMinusOne).times(basePowerTwo).idiv(2)
+  const thirdTerm = exp
+    .times(expMinusOne)
+    .times(expMinusTwo)
+    .times(basePowerThree)
+    .idiv(6)
+  return UNIT.plus(ratePerMilliSec.times(exp)).plus(secondTerm).plus(thirdTerm)
 }
 
-const UNIT_DECIMALS = 18
+const UNIT_DECIMALS = 27
 const UNIT = valueToZDBigNumber(10).pow(UNIT_DECIMALS)
 const HALF_UNIT = UNIT.dividedBy(2)
 
 const unitMul = (a: BigNumberValue, b: BigNumberValue): BigNumber =>
   HALF_UNIT.plus(valueToZDBigNumber(a).multipliedBy(b)).div(UNIT)
-
-const unitPow = (a: BigNumberValue, p: BigNumberValue): BigNumber => {
-  let x = valueToZDBigNumber(a)
-  let n = valueToZDBigNumber(p)
-  let z = n.modulo(2).eq(0) ? valueToZDBigNumber(UNIT) : x
-
-  for (n = n.div(2); !n.eq(0); n = n.div(2)) {
-    x = unitMul(x, x)
-
-    if (!n.modulo(2).eq(0)) {
-      z = unitMul(z, x)
-    }
-  }
-
-  return z
-}
