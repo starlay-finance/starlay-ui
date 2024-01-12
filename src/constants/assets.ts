003a5a80598c74f749e296034e201cd93971e43d
topdev <kevinlinsg1219@gmail.com>
2024-01-12 18:37:17 +0900

    remove ACA token from hero section

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import {
   SymbolAca,
   SymbolDot,
@@ -6,7 +7,7 @@ import {
 } from 'src/assets/images'
 import { Asset, AssetSymbol } from 'src/types/models'
 
-export const LISTED_ASSET_SYMBOLS = ['ACA', 'LDOT', 'USDC', 'DOT'] as const
+export const LISTED_ASSET_SYMBOLS = ['mACA', 'LDOT', 'USDC', 'DOT'] as const
 
 export const SYMBOL_ORDER: Record<AssetSymbol, number> =
   LISTED_ASSET_SYMBOLS.reduce(
@@ -18,8 +19,8 @@ export const SYMBOL_ORDER: Record<AssetSymbol, number> =
   ) as Record<AssetSymbol, number>
 
 export const ASSETS_DICT: { [key in AssetSymbol]: Asset } = {
-  ACA: {
-    symbol: 'ACA',
+  mACA: {
+    symbol: 'mACA',
     name: 'Acala',
     icon: SymbolAca,
   },
@@ -48,4 +49,4 @@ export const ASSETS_BY_ADDRESS_DICT: Partial<Record<string, Asset>> = {
   },
 } as const
 
-export const ASSETS: Asset[] = Object.values(ASSETS_DICT)
+export const ASSETS: Asset[] = Object.values(_.omit(ASSETS_DICT, 'mACA'))
